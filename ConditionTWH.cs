51676ebb4ef4fe1c1f5ded3013f97ade40d1f0b2
Snehasrg <sghongade642@gmail.com>
2023-01-05 00:30:11 +0530

    Total Working condition

---
+++ ConditionTWH.cs
@@ -0,0 +1,50 @@
+﻿namespace EmpWageDay9
+{
+    public class ConditionTWH
+    {
+        public void Check()
+        {
+            int Wage_Per_Hour = 20;
+            int Max_Days_IN_Month = 20;
+            int Max_HrsInMonth = 100;
+            int empHour;
+            int totalHours = 0;
+            int totalDays = 0;
+            ;
+            while (totalHours < Max_HrsInMonth || totalDays < Max_Days_IN_Month)
+            {
+                Random R = new Random();
+                int empCheck = R.Next(3);
+                totalDays++;
+                switch (empCheck)
+                {
+                    case 1:
+                        {
+                            //Console.WriteLine("Employee is present.");
+                            empHour = 8;
+                            break;
+                        }
+                    case 2:
+                        {
+                            //Console.WriteLine("Employee present PartTime.");
+                            empHour = 4;
+                            break;
+                        }
+                    default:
+                        {
+                            //Console.WriteLine("Employee is absent.");
+                            empHour = 0;
+                            break;
+                        }
+                }
+                totalHours = (totalHours + empHour);
+
+
+                Console.WriteLine("Total employee hours are :- " + totalHours);
+                int sallery = totalHours * Wage_Per_Hour;
+                Console.WriteLine("Total employee wage is :- " + sallery);
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
