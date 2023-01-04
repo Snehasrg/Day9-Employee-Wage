fc7092f8887cd69933801668bb11e6b1bb7fd5c9
Snehasrg <sghongade642@gmail.com>
2023-01-05 00:01:25 +0530

    Adding Parttime Wage

---
+++ PartTime.cs
@@ -0,0 +1,34 @@
+﻿namespace EmpWageDay9
+{
+    public class PartTime
+    {
+        public void Check()
+        {
+            int Is_Full_Time = 1;
+            int Is_Part_Time = 2;
+            int Wage_Per_Hour = 20;
+            int empHour;
+            int DailyEmpWage;
+            Random R = new Random();
+            int empCheck = R.Next(3);
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present Full time .");
+                empHour = 8;
+            }
+            else if (empCheck == Is_Part_Time)
+            {
+                Console.WriteLine("Employee present PartTime.");
+                empHour = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+                empHour = 0;
+            }
+            DailyEmpWage = Wage_Per_Hour * empHour;
+            Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
+
+        }
+    }
+}
