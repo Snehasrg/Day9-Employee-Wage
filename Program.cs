462898c0e8b743ad9793370df53c2915d75d33e5
Snehasrg <sghongade642@gmail.com>
2023-01-05 00:43:41 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args) 
         {
           //Console.WriteLine("Welcome To Employee Wage Computation Program");
-            ConditionTWH obj = new ConditionTWH();
+            ClassMethodEW obj = new ClassMethodEW();
             obj.Check();
         }
     }
