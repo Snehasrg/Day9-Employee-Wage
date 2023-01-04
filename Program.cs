10adf4d64ab2ced9e384b2cf3ed86a6c3eb71fcf
Snehasrg <sghongade642@gmail.com>
2023-01-04 23:16:56 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmpWageDay9
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+        
+        }
+    }
+}
