565fac98a3b9638a54b23f431f156a037dc85297
Snehasrg <sghongade642@gmail.com>
2023-01-05 00:19:06 +0530

    Monthly Wage

---
+++ MonthWage.cs
@@ -0,0 +1,50 @@
+﻿namespace EmpWageDay9
+{
+    public class MonthWage
+    {
+        public void Check() 
+        {
+            int Is_Full_Time = 1;
+            int Is_Part_Time = 2;
+            int Wage_Per_Hour = 20;
+            int Working_Days_IN_Month = 20;
+            int empHour;
+            int MonthlyEmpWages;
+            int TotalHrs = 0;
+
+
+            for (int i = 0; i < Working_Days_IN_Month; i++)
+            {
+                Random R = new Random();
+                int empCheck = R.Next(3);
+                switch (empCheck)
+                {
+                    case 1:
+                        {
+                            //  Console.WriteLine("Employee is present.");
+                            empHour = 8;
+                            break;
+                        }
+                    case 2:
+                        {
+                            // Console.WriteLine("Employee present PartTime.");
+                            empHour = 4;
+                            break;
+                        }
+                    default:
+                        {
+                            //  Console.WriteLine("Employee is absent.");
+                            empHour = 0;
+                            break;
+                        }
+
+                }
+
+                TotalHrs = TotalHrs + empHour;
+            }
+            Console.WriteLine("Total working hours in month are :- " + TotalHrs);
+            MonthlyEmpWages = TotalHrs * Wage_Per_Hour;
+            Console.WriteLine("Monthly employee wage is :- " + MonthlyEmpWages);
+        }
+    }
+}
