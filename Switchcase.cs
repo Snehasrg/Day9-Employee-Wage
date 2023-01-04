3750d42d3a2bd4c6ecd8a1e6b7115e20ec5b023e
Snehasrg <sghongade642@gmail.com>
2023-01-05 00:10:38 +0530

    Added using Switch Case

---
+++ Switchcase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageDay9
+{
+    public class Switchcase
+    {
+        public void Check()
+        {
+            int Is_Full_Time = 1;
+            int Is_Part_Time = 2;
+            int Wage_Per_Hour = 20;
+            int empHour;
+            int DailyEmpWage;
+            Random R = new Random();
+            int empCheck = R.Next(3);
+
+            switch (empCheck)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Employee is present.");
+                        empHour = 8;
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Employee present PartTime.");
+                        empHour = 4;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Employee is absent.");
+                        empHour = 0;
+                        break;
+                    }
+            }
+
+            DailyEmpWage = Wage_Per_Hour * empHour;
+            Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
+        }
+    }
+}
