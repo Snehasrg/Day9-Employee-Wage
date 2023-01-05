375b5717cf775e5a6df11c14c89c2008686a208d
Snehasrg <sghongade642@gmail.com>
2023-01-05 17:38:15 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@
     {
         public static void Main(string[] args) 
         {
-          //Console.WriteLine("Welcome To Employee Wage Computation Program");
-            MultipleComanyEW obj = new MultipleComanyEW();
-            obj.Employee("Google", 20, 15, 80);
-            Console.WriteLine("------------------------------------------------------");
-            obj.Employee("Microsoft", 25, 10, 60);
+            TotalWage obj = new TotalWage("Google", 20, 10, 50);
+            TotalWage obj1 = new TotalWage("Microsoft", 20, 10, 50);
+
+            obj.ComputeEmpWage();
+            Console.WriteLine(obj.Return());
+            obj1.ComputeEmpWage();
+            Console.WriteLine(obj1.Return());
         }
     }
 }
