59d0c062affa5b831478c75c5282dadd51aa996d
Snehasrg <sghongade642@gmail.com>
2023-01-05 17:00:07 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@
         public static void Main(string[] args) 
         {
           //Console.WriteLine("Welcome To Employee Wage Computation Program");
-            ClassMethodEW obj = new ClassMethodEW();
-            obj.Check();
+            MultipleComanyEW obj = new MultipleComanyEW();
+            obj.Employee("Google", 20, 15, 80);
+            Console.WriteLine("------------------------------------------------------");
+            obj.Employee("Microsoft", 25, 10, 60);
         }
     }
 }
